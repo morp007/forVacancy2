da8dcee8a65750707116aa56b357ebdb25141d35
morp007 <morp007@rambler.ru>
2017-01-24 19:41:36 +0300

    Add files via upload

---
+++ forVacancy2.sql
@@ -0,0 +1,22 @@
+use testDB;
+
+select
+	res.ProductId,
+	Count(res.id) 'Куплен первым Х раз'
+from
+(
+	select t2.id, t2.ProductId
+	from
+	(
+		select
+			CustomerId,
+			min(DateCreated) b
+		from Sales
+		group by CustomerId
+	) t1
+	left outer join Sales t2
+	on
+		t1.CustomerId = t2.CustomerId and
+		t1.b = t2.DateCreated
+) res
+group by res.ProductId
